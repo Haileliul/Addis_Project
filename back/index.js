6688732b0fdd7de50c4fb481f9bcfcf8be71cdd8
haile <hailetechlover@gmail.com>
2024-08-29 09:41:32 +0300

    mongoos database has been integrated sucess fully

--- back/index.js
+++ back/index.js
@@ -1,12 +1,21 @@
 // index.js
 const express = require("express");
+const connectDB = require("./src/config/dbConfig");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+// Define your routes here
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+// First, connect to the database, then start the server
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App running http://localhost:${port + 1}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Server not started due to DB connection error:", error);
+  });
---
+++ back/src/config/dbConfig.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+
+const uri = `mongodb+srv://${dbUsername}:${dbPassword}@music.sror1.mongodb.net/?retryWrites=true&w=majority&appName=music`;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(uri);
+    mongoose.connection.once("open", async () => {
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    throw error;
+  } finally {
+    // Comment out the disconnect if this is an application that runs continuously
+    // await mongoose.disconnect();
+  }
+}
+
+connectDB().catch(console.dir);
+
+module.exports = connectDB;
